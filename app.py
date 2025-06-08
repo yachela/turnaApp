e79ee545acc2abfa42b590b14e5573925de06fe4
yachela <laura.yachelini@davinci.edu.ar>
2025-06-08 09:30:51 -0300

    Corrijo CORS en todos los endpoints

--- app.py
+++ app.py
@@ -3,14 +3,18 @@ from flask_cors import CORS
 import sqlite3
 
 app = Flask(__name__)
-CORS(app)
-
+CORS(app) 
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
+    return response 
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
-
 
 @app.route('/profesionales', methods=['GET'])
 def get_profesionales():
@@ -18,7 +22,6 @@ def get_profesionales():
     profesionales = conn.execute('SELECT * FROM profesionales').fetchall()
     conn.close()
     return jsonify([dict(p) for p in profesionales])
-
 
 @app.route('/profesionales/<int:profesional_id>/servicios', methods=['GET'])
 def get_servicios_por_profesional(profesional_id):
@@ -30,6 +33,5 @@ def get_servicios_por_profesional(profesional_id):
     conn.close()
     return jsonify([dict(s) for s in servicios])
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)

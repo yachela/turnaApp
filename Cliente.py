000aae0c2eee2bfbf15ac326c75fa13e2a274f81
yachela <laura.yachelini@davinci.edu.ar>
2025-06-14 08:01:37 -0300

    Elimino archivos de versionado anterior que contenian las clases modelo

--- Cliente.py
+++
@@ -1,5 +0,0 @@
-class Cliente:
-    def __init__(self, id, nombre, email):
-        self.id = id
-        self.nombre = nombre
-        self.email = email
--- Turno.py
+++
@@ -1,8 +0,0 @@
-class Turno:
-    def __init__(self, id, fecha, hora, estado, cliente_id, servicio_id):
-        self.id = id
-        self.fecha = fecha  
-        self.hora = hora    
-        self.estado = estado  
-        self.cliente_id = cliente_id
-        self.servicio_id = servicio_id

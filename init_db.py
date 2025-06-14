83ff9ecb81d6bcf771f74879a55b6b8218774e62
yachela <laura.yachelini@davinci.edu.ar>
2025-06-14 08:04:29 -0300

    fix: corrijo errores de indentacion en init_db.py

--- init_db.py
+++ init_db.py
@@ -4,11 +4,11 @@ conn = sqlite3.connect('database.db')
 
 conn.execute('''
    CREATE TABLE IF NOT EXISTS profesionales (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT NOT NULL,
-    especialidad TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE
-);
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+     nombre TEXT NOT NULL,
+     especialidad TEXT NOT NULL,
+     email TEXT NOT NULL UNIQUE
+   );
 ''')
 
 profesionales = [
@@ -22,29 +22,18 @@ profesionales = [
 ]
 
 conn.executemany(
-    "INSERT OR IGNORE INTO profesionales (nombre, especialidad, email) VALUES (?, ?, ?)", profesionales)
+    "INSERT OR IGNORE INTO profesionales (nombre, especialidad, email) VALUES (?, ?, ?)",
+    profesionales
+)
 
 conn.execute('''
     CREATE TABLE IF NOT EXISTS servicios (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nombre TEXT NOT NULL,
-        duracion INTEGER NOT NULL, -- en minutos
-        precio REAL NOT NULL,
-        profesional_id INTEGER NOT NULL,
-        FOREIGN KEY (profesional_id) REFERENCES profesionales(id)
-    );
-''')
-
-conn.execute('''
-    CREATE TABLE IF NOT EXISTS turnos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        profesional_id INTEGER NOT NULL,
-        cliente_id INTEGER NOT NULL,
-        servicio_id INTEGER NOT NULL,
-        fecha TEXT NOT NULL,
-        hora TEXT NOT NULL,
-        FOREIGN KEY (profesional_id) REFERENCES profesionales(id),
-        FOREIGN KEY (servicio_id) REFERENCES servicios(id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre TEXT NOT NULL,
+    duracion INTEGER NOT NULL,
+    precio REAL NOT NULL,
+    profesional_id INTEGER NOT NULL,
+    FOREIGN KEY (profesional_id) REFERENCES profesionales(id)
     );
 ''')
 
@@ -55,10 +44,22 @@ servicios = [
     ('Rehabilitación postural', 60, 4500, 3)
 ]
 
-conn.executemany('''
-    INSERT INTO servicios (nombre, duracion, precio, profesional_id)
-    VALUES (?, ?, ?, ?)
-''', servicios)
+conn.executemany(
+    'INSERT OR IGNORE INTO servicios (nombre, duracion, precio, profesional_id) VALUES (?, ?, ?, ?)',
+    servicios
+)
+conn.execute('''
+    CREATE TABLE IF NOT EXISTS turnos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    profesional_id INTEGER NOT NULL,
+    cliente_id INTEGER NOT NULL,
+    servicio_id INTEGER NOT NULL,
+    fecha TEXT NOT NULL,
+    hora TEXT NOT NULL,
+    FOREIGN KEY (profesional_id) REFERENCES profesionales(id),
+    FOREIGN KEY (servicio_id) REFERENCES servicios(id)
+    );
+''')
 
 conn.commit()
 conn.close()
